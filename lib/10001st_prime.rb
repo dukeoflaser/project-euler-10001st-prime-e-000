dd97b019091bf6a04ba0f249f7e923f7f7b86105
Nathaniel Miller <dukeoflaser@gmail.com>
2016-01-03 19:37:31 +0000

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,24 @@
 # Implement your procedural solution here!
+
+def prime_number_for(nth)
+  max = nth * 11            #A bit arbitrary and fails on larger numbers
+  numbers = (2..max).to_a
+  upper = Math.sqrt(max)
+  
+  (2..upper).each do |n|
+    unless numbers[n - 2] == nil
+      (n * n).step(max, n) { |i| numbers[i - 2] = nil }
+    end
+  end
+
+  numbers.compact[nth - 1]
+end
+
+
+## Or use Ruby's built in Prime class:
+# require 'prime'
+
+# def prime_number_for(nth)
+#   Prime.take(nth).last
+# end
+  
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+
+class Prime
+  
+  def initialize(nth)
+    @nth = nth
+  end
+  
+  def number
+    max = @nth * 11           #A bit arbitrary and fails on larger numbers
+    numbers = (2..max).to_a
+    upper = Math.sqrt(max)
+    
+    (2..upper).each do |n|
+      unless numbers[n - 2] == nil
+        (n * n).step(max, n) { |i| numbers[i - 2] = nil }
+      end
+    end
+  
+    numbers.compact[@nth - 1]
+  end
+  
+  ## Or use Ruby's built in Prime class:
+  # require 'prime'
+  
+  # def number(nth)
+  #   Prime.take(nth).last
+  # end
+  
+end
